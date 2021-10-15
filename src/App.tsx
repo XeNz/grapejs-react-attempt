961fef7571afa7b925cde2c469f1ece86ad2e4fc
Alexander Willemsen <awm@foreach.be>
2021-10-15 09:07:45 +0200

    fix typo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css';
 import 'grapesjs/dist/css/grapes.min.css';
 // @ts-ignore
-import Mailbody from './MailBody';
+import MailBody from './MailBody';
 import MJMLComponent from './MJMLComponent';
 import { MJMLEditorConfig } from './MJMLEditorConfig';
 
 function App() {
-  return <MJMLComponent editorConfig={MJMLEditorConfig} mjmlContent={Mailbody} ></MJMLComponent>;
+  return <MJMLComponent editorConfig={MJMLEditorConfig} mjmlContent={MailBody} ></MJMLComponent>;
 }
 
 export default App;
